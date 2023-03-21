cba7418a4239ffdd3a9df2d0ca5f293b08a456be
cl9bix <126515747+cl9bix@users.noreply.github.com>
2023-03-21 17:29:02 +0100

    Update dz7.py

--- dz7.py
+++ dz7.py
@@ -1,15 +1,28 @@
-from setuptools import setup, find_packages
+import argparse
+import os
+import shutil
 
-setup(
-    name="clean_folder",
-    version="1.0.0",
-    author="Ваше ім'я",
-    author_email="ваша електронна пошта",
-    description="Python-пакет для очищення папки",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'clean-folder = clean_folder.clean:main'
-        ]
-    },
-)
+def main():
+    parser = argparse.ArgumentParser(description='Clean up folder')
+    parser.add_argument('path', metavar='path', type=str, help='Path to folder')
+
+    args = parser.parse_args()
+    path = args.path
+
+    if not os.path.exists(path):
+        print(f'Error: Path "{path}" does not exist')
+        return
+
+    if not os.path.isdir(path):
+        print(f'Error: "{path}" is not a directory')
+        return
+
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Error: Failed to delete "{file_path}". Reason: {e}')
